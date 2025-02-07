8e273ff6946b5959838b0e2e7eeee6fc0477a2c9
Jose Luis Cáceres <caceresjoselu@gmail.com>
2025-02-07 19:26:28 +0100

    Leer y Crear :)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("tupac")
+var texto=""
+var vector=[]
+
+function guardar(){
+    var nombre=document.getElementById("cajaNombre").value
+    vector.push(nombre)
+    document.getElementById("cajaNombre").value=""
+    actualizarTabla()
+}
+function actualizarTabla(){
+    texto=""
+
+    for (var i=0;i<vector.length;i++){
+        var fila= "<tr>" +"<td>" + vector[i] + "</td>" +"</tr>" 
+        texto+=fila
+        }
+        document.getElementById("tabla").innerHTML=texto
+}
